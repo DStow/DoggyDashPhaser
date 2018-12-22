b76d06a662cd735d71c2b190a772c33d337e8a67
Dan Stow <Dan_Stow@live.co.uk>
2018-12-22 23:53:20 +0000

    Add a utilities file with a fixed timer class

---
+++ src/js/utilities.js
@@ -0,0 +1,25 @@
+class fixedTimer {
+	constructor(gameState, seconds, enabled) {
+		this.gameState = gameState;
+
+		this.enabled = enabled;
+		this.targetSeconds = seconds;
+		this.elapsedTime = 0;
+		this.tick = false;
+	}
+
+	update(elapsedSeconds) {
+		// Only tick once
+		if(this.tick) { this.tick = false; }
+
+		if(this.enabled) {
+			this.elapsedTime += elapsedSeconds;
+
+			if(this.elapsedTime >= this.targetSeconds) {
+				this.tick = true;
+				// Keep the overflow
+				this.elapsedTime -= this.targetSeconds;
+			}
+		}
+	}
+}
